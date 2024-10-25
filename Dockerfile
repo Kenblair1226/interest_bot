3dd15bcb466ec47670cc87bb97ef1c0a0c3d423e
Ken <ken.cc.chen@gogoro.com>
2024-10-25 13:50:38 +0800

    add gitlab-ci

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,15 @@
 # Use the official Golang image as a parent image
-FROM golang:1.20-alpine
+FROM golang:1.22-alpine
 
 # Set the working directory inside the container
 WORKDIR /app
 
 # Copy go mod and sum files
 COPY go.mod go.sum ./
+
+RUN apk add --no-cache gcc musl-dev
+
+ARG CGO_ENABLED=1
 
 # Download all dependencies
 RUN go mod download
@@ -14,7 +18,11 @@ RUN go mod download
 COPY src/ ./src/
 
 # Build the application
-RUN go build -o /interest_bot src/*.go
+# RUN go build -o /interest_bot src/*.go
 
 # Run the application
-CMD ["/interest_bot"]
+# CMD ["/interest_bot"]
+
+EXPOSE 8080
+
+CMD ["go", "run", "./src"]
--- src/main.go
+++ src/main.go
@@ -7,19 +7,28 @@ import (
 	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
 )
 
 const checkInterval = 15 * time.Minute
 
 func main() {
-	telegramToken := os.Getenv("TELEGRAM_TOKEN")
-	if telegramToken == "" {
-		log.Fatal("TELEGRAM_TOKEN environment variable is not set")
+	// Try to load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found, will use OS environment variables")
 	}
 
-	chatIDStr := os.Getenv("CHAT_ID")
+	// Get Telegram token
+	telegramToken := getEnv("TELEGRAM_TOKEN", "")
+	if telegramToken == "" {
+		log.Fatal("TELEGRAM_TOKEN is not set")
+	}
+
+	// Get Chat ID
+	chatIDStr := getEnv("CHAT_ID", "")
 	if chatIDStr == "" {
-		log.Fatal("CHAT_ID environment variable is not set")
+		log.Fatal("CHAT_ID is not set")
 	}
 
 	chatID, err := strconv.ParseInt(chatIDStr, 10, 64)
@@ -76,4 +85,15 @@ func joinStrings(strings []string, separator string) string {
 		result += s
 	}
 	return result
+}
+
+// getEnv retrieves the value of the environment variable named by the key.
+// It returns the value, which will be empty if the variable is not present.
+// If the variable is not present and a default value is given, it returns the default value.
+func getEnv(key, defaultValue string) string {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		return defaultValue
+	}
+	return value
 }
